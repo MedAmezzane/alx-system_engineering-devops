d617b87a2a0a16ff394ddbe420b0a59d17c92f4a
Med AMEZZANE <Med.AMEZZANE@gmail.com>
2024-03-07 01:27:31 +0100

    V2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,22 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    """
+    Return the total number of subscribers on a given subreddit.
+
+    :param subreddit: The name of the subreddit.
+    :return: The total number of subscribers, or 0 if the input is invalid.
+    """
+    # Check if the subreddit is a valid string
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
+
+    # Make a GET request to the Reddit API to fetch subreddit information
+    api_url = f'http://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0'}
+    response = requests.get(api_url, headers=headers).json()
+
+    # Extract the number of subscribers from the API response
+    subscribers = response.get("data", {}).get("subscribers", 0)
+
+    return subscribers
