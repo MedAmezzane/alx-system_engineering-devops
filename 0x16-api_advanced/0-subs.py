1c7fbb5a57744723a39897decd5e6bbf6349a961
Med AMEZZANE <Med.AMEZZANE@gmail.com>
2024-03-07 01:31:39 +0100

    V3

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
 """
-Script that queries subscribers on a given Reddit subreddit.
+Function that queries the Reddit API and returns
+the number of subscribers for a given subreddit.
 """
-
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """
-    Return the total number of subscribers on a given subreddit.
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
 
-    :param subreddit: The name of the subreddit.
-    :return: The total number of subscribers, or 0 if the input is invalid.
-    """
-    # Check if the subreddit is a valid string
-    if subreddit is None or not isinstance(subreddit, str):
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code != 200:
         return 0
-
-    # Make a GET request to the Reddit API to fetch subreddit information
-    api_url = f'http://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0'}
-    response = requests.get(api_url, headers=headers).json()
-
-    # Extract the number of subscribers from the API response
-    subscribers = response.get("data", {}).get("subscribers", 0)
-
-    return subscribers
+    dic = res.json()
+    if 'data' not in dic:
+        return 0
+    if 'subscribers' not in dic.get('data'):
+        return 0
+    return res.json()['data']['subscribers']
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,36 +1,36 @@
 #!/usr/bin/python3
 """
-Script to print hot posts on a given Reddit subreddit.
+Function that queries the Reddit API and prints
+the top ten hot posts of a subreddit
 """
-
 import requests
+import sys
+
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    # Construct the URL for the subreddit's hot posts in JSON format
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
 
-    # Define headers for the HTTP request, including User-Agent
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        'User-Agent': u_agent
     }
 
-    # Define parameters for the request, limiting the number of posts to 10
     params = {
-        "limit": 10
+        'limit': 10
     }
 
-    # Send a GET request to the subreddit's hot posts page
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-
-    # Check if the response status code indicates a not-found error (404)
-    if response.status_code == 404:
-        print("None")
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    res = requests.get(url,
+                       headers=headers,
+                       params=params,
+                       allow_redirects=False)
+    if res.status_code != 200:
+        print(None)
         return
-
-    # Parse the JSON response and extract the 'data' section
-    results = response.json().get("data")
-
-    # Print the titles of the top 10 hottest posts
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    dic = res.json()
+    hot_posts = dic['data']['children']
+    if len(hot_posts) is 0:
+        print(None)
+    else:
+        for post in hot_posts:
+            print(post['data']['title'])
